d3b762fb6b11d937ad764c89d8c97eaadddd1dbf
RomanBudko <romanjaneiro@mail.ru>
2020-05-19 01:36:44 +0400

    Добавлен финальный штрих ReadLine
    Сделано для того, чтобы консоль не закрывалась так быстро

--- Tasks/1.1.1 RECTANGLE/Program.cs
+++ Tasks/1.1.1 RECTANGLE/Program.cs
@@ -20,6 +20,8 @@ namespace _1._1._1_RECTANGLE
             CheckOfNumber(ref bSide);
 
             CountOfRectangleArea(aSide, bSide);
+
+            Console.ReadLine();
         }
         //Метод для проверки введенного числа на корректность
 
