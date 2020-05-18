54e7dc802074581138ead337e0e6c46b7ce41ce9
RomanBudko <romanjaneiro@mail.ru>
2020-05-19 02:26:37 +0400

    Чек поинт второго задания.

--- Tasks/1.1.2. TRIANGLE/Program.cs
+++ Tasks/1.1.2. TRIANGLE/Program.cs
@@ -7,6 +7,20 @@ namespace _1._1._2.TRIANGLE
     {
         static void Main(string[] args)
         {
+            int numberOfDrop;
+
+            if(int.TryParse(Console.ReadLine(), out int x))
+            {
+                numberOfDrop = x;
+            }
+            
+            char star = '*';
+
+            for(int i = 0; i <= numberOfDrop; i++)
+            {
+
+            }
+
         }
     }
 }
