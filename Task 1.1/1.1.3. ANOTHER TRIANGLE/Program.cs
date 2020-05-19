53e8bc31ce91bf3405902818274b3e64946e027d
RomanBudko <romanjaneiro@mail.ru>
2020-05-19 07:38:02 +0400

    Сделано третье задание

--- Task 1.1/1.1.3. ANOTHER TRIANGLE/Program.cs
+++ Task 1.1/1.1.3. ANOTHER TRIANGLE/Program.cs
@@ -10,6 +10,37 @@ namespace _1._1._3.ANOTHER_TRIANGLE
     {
         static void Main(string[] args)
         {
+            int numberOfDrop = 0;
+            int secondNumber = 0;
+            //Проверка на корректность ввода
+            if(int.TryParse(Console.ReadLine(), out int x))
+            {
+                numberOfDrop = x;
+            }
+            else
+            {
+                Console.WriteLine("Введите корректное число");
+            }
+            //Рисовка треугольника
+            if(numberOfDrop > 0)
+            {
+                for (int i = 0; i < numberOfDrop; i++)
+                {
+                    for(int j = numberOfDrop - i; j > 0; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for(int k = 0; k <= 2 * i; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введите корректное число");
+            }
         }
     }
 }
