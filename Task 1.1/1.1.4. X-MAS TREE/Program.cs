0874d41dad09aa564a6a0269f183f2e8368d5f86
RomanBudko <romanjaneiro@mail.ru>
2020-05-19 07:50:01 +0400

    Создал и перенес новый проект в папку с заданиями

---
+++ Task 1.1/1.1.4. X-MAS TREE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._4.X_MAS_TREE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
