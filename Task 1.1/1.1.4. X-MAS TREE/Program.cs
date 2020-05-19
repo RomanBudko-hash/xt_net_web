e1ffe5fd7c8b8ceb76ee00f26eada075e84b6abc
RomanBudko <romanjaneiro@mail.ru>
2020-05-20 03:18:31 +0400

    Добавлено задание с Елочкой

--- Task 1.1/1.1.4. X-MAS TREE/Program.cs
+++ Task 1.1/1.1.4. X-MAS TREE/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _1._1._4.X_MAS_TREE
+ 
+public class Test
 {
-    class Program
-    {
-        static void Main(string[] args)
+	public static void Main()
+	{
+		Console.ForegroundColor = ConsoleColor.Green;
+
+		var numberOflevelTree = 0;
+
+		if(int.TryParse(Console.ReadLine(), out int x))
         {
+			numberOflevelTree = x;
         }
-    }
+        else
+        {
+        Console.WriteLine("Введите корректное число");
+		}
+		if (numberOflevelTree > 0)
+		{
+			for (int i = 1; i <= numberOflevelTree; i++)
+			{
+		    for (int j = 0; j < i; j++)
+		    {
+		        string branch = new String('*', j);
+		        Console.WriteLine(branch.PadLeft(numberOflevelTree + 3) + "*" + branch);
+		    }
+			}
+		}
+	}
 }
