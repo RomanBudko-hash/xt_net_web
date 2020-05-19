324d11a8a7c2ea0dee4eb07949d47467ce91195c
RomanBudko <romanjaneiro@mail.ru>
2020-05-19 06:11:48 +0400

    Сделано второе задание

--- Tasks/1.1.2. TRIANGLE/Program.cs
+++ Tasks/1.1.2. TRIANGLE/Program.cs
@@ -13,13 +13,30 @@ namespace _1._1._2.TRIANGLE
             {
                 numberOfDrop = x;
             }
-            
-            char star = '*';
-
-            for(int i = 0; i <= numberOfDrop; i++)
+            else
             {
+                Console.WriteLine("Введите корректное число");
             }
-
+            
+            if(numberOfDrop > 0)
+            {
+                for(int i = 1; i <= numberOfDrop; i++)
+                {
+                    for (int j = 1; j <= numberOfDrop - i; j++)
+                    {
+                    Console.Write("");
+                    }
+                    for(int k = 1; k <= i; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine("");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введите корректное число");
+            }
         }
     }
 }
