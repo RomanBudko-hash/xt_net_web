ee3a89de5410826cedbcb27bee9d9ad07b36479e
RomanBudko <romanjaneiro@mail.ru>
2020-05-20 05:08:44 +0400

    Добавлены комментарии для лучшего понимания плохого почерка кода

--- Task 1.1/1.1.5. SUM OF NUMBERS/Program.cs
+++ Task 1.1/1.1.5. SUM OF NUMBERS/Program.cs
@@ -7,33 +7,32 @@ namespace _1._1._5.SUM_OF_NUMBERS
     {
         static void Main(string[] args)
         {
-            int [] targetNumber = new int [999];
+            int [] targetNumber = new int [999];// Массив для хранения всех чисел в выборке
 
-            int[] filtrNumber = new int[999];
+            int[] filtrNumber = new int[999]; // Массив для отфильтрованных чисел
 
-            int x = 0;
+            int x = 0; // Переменная для удобства отсеивания
 
-            int countofFiltrNumbers = 0;
+            int countofFiltrNumbers = 0; // Итоговая сумма всех чисел
 
-            for (int i = 1; i < targetNumber.Length; i++)
+            for (int i = 1; i < targetNumber.Length; i++)//Заполнение массива натуральными числами
             {
                 targetNumber[0] = 1;
                 targetNumber[i] = i+1;
 
-                if (i % 3 == 0 || i % 5 == 0)
+                if (i % 3 == 0 || i % 5 == 0) //Отсеивание кратных 3м и 5и чисел
                 {
                     filtrNumber[x] = i;
                 }
                 x++;
             }
-            for(int j = 0; j < filtrNumber.Length; j++)
+
+            for(int j = 0; j < filtrNumber.Length; j++)//Нахождение итогового числа
             {
-                countofFiltrNumbers = 
+                countofFiltrNumbers += filtrNumber[j];
             }
             
-            Console.WriteLine(string.Join(" ", targetNumber));
-            Console.WriteLine();
-            Console.WriteLine(string.Join(" ", filtrNumber));
+            Console.WriteLine(countofFiltrNumbers);
             Console.ReadLine();
         }
     }
