957d5df337eb8410ab271aa745b0db52ccc831be
RomanBudko <romanjaneiro@mail.ru>
2020-05-20 05:53:15 +0400

    Начато решение задачи

--- Task 1.1/1.1.6. FONT ADJUSTMENT/Program.cs
+++ Task 1.1/1.1.6. FONT ADJUSTMENT/Program.cs
@@ -5,8 +5,18 @@ namespace _1._1._6.FONT_ADJUSTMENT
 {
     class Program
     {
+        enum ParametrOftitle
+        {
+            bold = 1,
+            italic,
+            underline
+        }
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Параметры надписи: None");
+            Console.WriteLine("Введите:");
         }
     }
 }
