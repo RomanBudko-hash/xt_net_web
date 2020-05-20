8081a32d8207312ce3baca7628a382f20e3c6ba3
RomanBudko <romanjaneiro@mail.ru>
2020-05-20 04:57:55 +0400

    Прописаана основная логика приложения

--- Task 1.1/1.1.5. SUM OF NUMBERS/Program.cs
+++ Task 1.1/1.1.5. SUM OF NUMBERS/Program.cs
@@ -9,13 +9,31 @@ namespace _1._1._5.SUM_OF_NUMBERS
         {
             int [] targetNumber = new int [999];
 
+            int[] filtrNumber = new int[999];
+
+            int x = 0;
+
+            int countofFiltrNumbers = 0;
+
             for (int i = 1; i < targetNumber.Length; i++)
             {
                 targetNumber[0] = 1;
                 targetNumber[i] = i+1;
-            }
 
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    filtrNumber[x] = i;
+                }
+                x++;
+            }
+            for(int j = 0; j < filtrNumber.Length; j++)
+            {
+                countofFiltrNumbers = 
+            }
+            
             Console.WriteLine(string.Join(" ", targetNumber));
+            Console.WriteLine();
+            Console.WriteLine(string.Join(" ", filtrNumber));
             Console.ReadLine();
         }
     }
