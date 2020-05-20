4c1a61088f3e37009e530f3bda68cc169d0b2c55
RomanBudko <romanjaneiro@mail.ru>
2020-05-21 02:27:07 +0400

    Создание очень плохого кода закончено
    Переделать, когда будет время

--- Task 1.1/1.1.6. FONT ADJUSTMENT/Program.cs
+++ Task 1.1/1.1.6. FONT ADJUSTMENT/Program.cs
@@ -17,32 +17,22 @@ namespace _1._1._6.FONT_ADJUSTMENT
             Console.ForegroundColor = ConsoleColor.Green;
 
             TypeOfWrite tow;
-
+            TypeOfWrite wot;
             tow = TypeOfWrite.None;
+            wot = TypeOfWrite.bold;
 
             EnterAtTheConsole(tow);
-
-            switch (int.Parse(Console.ReadLine()))
-            {
-                case 1:
-                    tow = TypeOfWrite.bold;
-                    break;
-                case 2:
-                    tow = TypeOfWrite.italic;
-                    break;
-                case 3:
-                    tow = TypeOfWrite.underline;
-                    break;
-                default:
-                    Console.WriteLine("Введено некорректное значение");
-                    break;
-            }
+            Check(out tow);
+            EnterAtTheConsole(tow);
+            Check(out wot);
+            EnterAtTheConsole(tow, wot);
+            Check(out tow);
             EnterAtTheConsole(tow);
         }
 
         public static string s = "";
 
-        public static void EnterAtTheConsole(TypeOfWrite x)
+        public static void EnterAtTheConsole(TypeOfWrite x)// Метод для отображения в консоль с 1 параметром
         {
             Console.WriteLine("Параметры надписи: " + x);
             Console.WriteLine("Введите:");
@@ -50,7 +40,32 @@ namespace _1._1._6.FONT_ADJUSTMENT
             Console.WriteLine(s.PadRight(7) + "2: " + TypeOfWrite.italic);
             Console.WriteLine(s.PadRight(7) + "3: " + TypeOfWrite.underline);
         }
-
-
+        public static void EnterAtTheConsole(TypeOfWrite x, TypeOfWrite y)// Метод для отображения в консоль с 2 параметрами
+        {
+            Console.WriteLine("Параметры надписи: " + x + " " + y);
+            Console.WriteLine("Введите:");
+            Console.WriteLine(s.PadRight(7) + "1: " + TypeOfWrite.bold);
+            Console.WriteLine(s.PadRight(7) + "2: " + TypeOfWrite.italic);
+            Console.WriteLine(s.PadRight(7) + "3: " + TypeOfWrite.underline);
+        }
+        public static void Check(out TypeOfWrite x) // Проверка ввода
+        {
+            x = 0;
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    x = TypeOfWrite.bold;
+                    break;
+                case 2:
+                    x = TypeOfWrite.italic;
+                    break;
+                case 3:
+                    x = TypeOfWrite.underline;
+                    break;
+                default:
+                    Console.WriteLine("Введено некорректное значение");
+                    break;
+            }
+        }
     }
 }
