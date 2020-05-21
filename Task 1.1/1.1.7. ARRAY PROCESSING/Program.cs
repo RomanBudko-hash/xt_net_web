d73fde9e9ea35a0e239040054e3ed7974a1b0a6f
RomanBudko <romanjaneiro@mail.ru>
2020-05-21 13:52:43 +0400

    Определены минимумы и максимумы

--- Task 1.1/1.1.7. ARRAY PROCESSING/Program.cs
+++ Task 1.1/1.1.7. ARRAY PROCESSING/Program.cs
@@ -11,29 +11,40 @@ namespace _1._1._7.ARRAY_PROCESSING
 
             var rand = new Random();
 
+            int max = 0;
+            int min = 1000;
+
             for (int i = 0; i < array.Length; i++)// Определение рандомных чисел для массива
             {
                 array[i] = rand.Next(1000);
             }
             Console.WriteLine(string.Join(" ", array));
 
-            for (int i = 1; i < array.Length; i++)// Определение максимального числа
+            for (int i = 0; i < array.Length - 1; i++)// Определение максимального числа
             {
-                if (array[i] < array[i-1])
+                if (array[i] > array[i+1] && array[i] > max)
                 {
-                    array[i] = array[i-1];
+                    max = array[i];
+                }
+                if(array[i] < array[i+1] && array[i+1] > max)
+                {
+                    max = array[i + 1];
                 }
             }
-            Console.WriteLine("Максимальное число = " + array[array.Length - 1]);
+            Console.WriteLine("Максимальное число = " + max);
 
-            for (int i = 1; i < array.Length; i++)// Определение минимального числа
+            for (int i = 0; i < array.Length - 1; i++)// Определение минимального числа
             {
-                if (array[i] > array[i - 1])
+                if (array[i] < array[i + 1] && array[i] < min)
                 {
-                    array[i] = array[i - 1];
+                    min = array[i];
                 }
+                if (array[i] > array[i + 1] && array[i + 1] < min)
+                {
+                    min = array[i + 1];
+                }
+                Console.WriteLine("Минимальное число = " + min);
             }
-            Console.WriteLine("Минимальное число = " + array[array.Length - 1]);
         }
     }
 }
