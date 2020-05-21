c802ee19823498845ad2fd9567bd380b1549c574
RomanBudko <romanjaneiro@mail.ru>
2020-05-21 04:25:50 +0400

    Выполнена половина задания

--- Task 1.1/1.1.7. ARRAY PROCESSING/Program.cs
+++ Task 1.1/1.1.7. ARRAY PROCESSING/Program.cs
@@ -7,6 +7,26 @@ namespace _1._1._7.ARRAY_PROCESSING
     {
         static void Main(string[] args)
         {
+            int [] array = new int[10];
+
+            var rand = new Random();
+
+            for (int i = 0; i < array.Length; i++)// Определение рандомных чисел для массива
+            {
+                array[i] = rand.Next(1000);
+            }
+            Console.WriteLine(string.Join(" ", array));
+
+            for (int i = 1; i < array.Length; i++)// Определение максимального числа
+            {
+                if (array[i] < array[i-1])
+                {
+                    array[i] = array[i-1];
+                }
+            }
+            Console.WriteLine("Максимальное число = " + array[array.Length - 1]);
+
+            
         }
     }
 }
