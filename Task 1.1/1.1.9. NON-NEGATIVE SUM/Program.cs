1cabc51394bfca8da6b3376b864639d5c07141d9
RomanBudko <romanjaneiro@mail.ru>
2020-05-23 02:02:52 +0400

    Задание сделано

---
+++ Task 1.1/1.1.9. NON-NEGATIVE SUM/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace _1._1._9.NON_NEGATIVE_SUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {0,1,2,0,4,5,101,7,0,9};
+
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    sum += array[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
