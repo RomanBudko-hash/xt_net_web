abd90923309901c13393f374b8b8fa12a1be5566
RomanBudko <romanjaneiro@mail.ru>
2020-05-23 02:19:11 +0400

    Задание сделано

---
+++ Task 1.1/1.1.10. 2D ARRAY/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace _1._1._10._2D_ARRAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[3,4];
+
+            int sum = 0;
+            
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        sum++;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
