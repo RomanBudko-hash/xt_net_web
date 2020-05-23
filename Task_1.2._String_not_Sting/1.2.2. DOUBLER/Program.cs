705905bff47936dcb0f731bebd32745efd2de588
RomanBudko <romanjaneiro@mail.ru>
2020-05-23 18:51:46 +0400

    Добавлена проверка введенных значений на корректность

--- Task_1.2._String_not_Sting/1.2.2. DOUBLER/Program.cs
+++ Task_1.2._String_not_Sting/1.2.2. DOUBLER/Program.cs
@@ -7,6 +7,21 @@ namespace _1._2._2.DOUBLER
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите пример строки");
+
+            string textEnter = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(textEnter))
+            {
+                Console.WriteLine("Введенная строка не соответствует требованиям");
+            }
+            string doublerFirstRow = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(doublerFirstRow))
+            {
+                Console.WriteLine("Введенная строка не соответствует требованиям");
+            }
+
         }
     }
 }
