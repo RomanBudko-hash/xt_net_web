b50fe4659f49cd559b9fe729832fcb9bc198290d
RomanBudko <romanjaneiro@mail.ru>
2020-05-24 01:48:29 +0400

    Создан проект для решения задачи

---
+++ Task_1.2._String_not_Sting/1.2.3. LOWERCASE/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._3.LOWERCASE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstRow = "Антон хорошо начал утро: послушал Стинга, выпил кофе и посмотрел Звёздные Войны";
+
+        }
+    }
+}
