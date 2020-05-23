34c52341356bf4f5705695c83428db371cec5852
RomanBudko <romanjaneiro@mail.ru>
2020-05-23 05:00:13 +0400

    Создан проект для решения задачи

---
+++ Task_1.2._String_not_Sting/1.2.1. AVERAGES/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace _1._2._1.AVERAGES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textExample = Console.ReadLine();
+
+            char [] separators = new char [] { '_', '-', ':', ';', '.', ',', '!', '?', ' ' };
+
+            var x = textExample.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string item in x)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
