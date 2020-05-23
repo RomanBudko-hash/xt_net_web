01a83aee38d84522cf6adde61a39bb926b3efd05
RomanBudko <romanjaneiro@mail.ru>
2020-05-23 18:13:17 +0400

    Добавлены комментарии
    Для лучшего понимания плохого почерка кода
    оставил комментарии

--- Task_1.2._String_not_Sting/1.2.1. AVERAGES/Program.cs
+++ Task_1.2._String_not_Sting/1.2.1. AVERAGES/Program.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 
-
 namespace _1._2._1.AVERAGES
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // При дробном значении дробная часть опускается
+
             string textExample = "Викентий хорошо отметил день рождения: покушал пиццу, посмотрел кино, пообщался со студентами в чате";
 
             char [] separators = new char [] { '_', '-', ':', ';', '.', ',', '!', '?', ' ' };
 
-            string [] completeString = textExample.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string [] completeString = textExample.Split(separators, StringSplitOptions.RemoveEmptyEntries); // Массив строк без знаков препинания
 
-            Console.WriteLine(completeString.Length); // Количество слов
+            Console.WriteLine("Количество слов = " + completeString.Length); // Количество слов
 
+            string singleRow = string.Join("", completeString); // Преобразование массива строк в одну строку без разделителей
+
+            Console.WriteLine("Количество символов = " + singleRow.Length); // Количество символов в строке
+
+            Console.WriteLine("Среднее количество знаков в слове = " + (singleRow.Length/completeString.Length)); // Вывод в консоль среднего знначения знаков в строке
         }
     }
 }
