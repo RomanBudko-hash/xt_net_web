a15092b0b5b93e4734d25f2a5a6fc5314bbb7fdf
RomanBudko <romanjaneiro@mail.ru>
2020-05-25 00:57:18 +0400

    Доделано задание, оставлены комментарии

--- Task_1.2._String_not_Sting/1.2.3. LOWERCASE/Program.cs
+++ Task_1.2._String_not_Sting/1.2.3. LOWERCASE/Program.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Был выбран пример со *
 namespace _1._2._3.LOWERCASE
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string firstRow = "Антон хорошо начал утро: послушал Стинга, выпил кофе и посмотрел Звёздные Войны";
+            string theMainRow = "Антон хорошо начал утро: послушал Стинга, выпил кофе и посмотрел Звёздные Войны";
 
+            char[] separators = new char[] { '_', '-', ':', ';', '.', ',', '!', '?', ' ' };
+
+            string[] breakTheMainRow = theMainRow.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            int result = 0;
+
+            for (int i = 0; i < breakTheMainRow.Length; i++) // Цикл, проверяющий является ли слово в массиве большебуквеннным
+            {
+                if (char.IsLower(breakTheMainRow[i][0]))
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
