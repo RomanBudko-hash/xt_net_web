57653fa2c936cc7c4a4cddf63b5cf7a9f994543c
RomanBudko <romanjaneiro@mail.ru>
2020-05-25 01:08:35 +0400

    Создание проекта

---
+++ Task_1.2._String_not_Sting/1.2.4. VALIDATOR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._4.VALIDATOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
