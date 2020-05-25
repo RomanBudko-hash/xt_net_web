ae8711b5dd8ca20a45b7bf78917561b749b79bc3
RomanBudko <romanjaneiro@mail.ru>
2020-05-25 20:48:55 +0400

    Проект сделан, осталось внести небольшие правки

--- Task_1.2._String_not_Sting/1.2.4. VALIDATOR/Program.cs
+++ Task_1.2._String_not_Sting/1.2.4. VALIDATOR/Program.cs
@@ -10,6 +10,23 @@ namespace _1._2._4.VALIDATOR
     {
         static void Main(string[] args)
         {
+            string enter = "я плохо учил русский язык. забываю начинать предложения с заглавной. хорошо, что можно написать программу!";
+
+            char[] enterInCharArray = enter.ToCharArray();
+
+            for (int i = 0; i < enterInCharArray.Length - 2; i++)
+            {
+                if (enterInCharArray[i].Equals('.')
+                 || enterInCharArray[i].Equals('!')
+                 || enterInCharArray[i].Equals('?'))
+                {
+                    enterInCharArray[i + 2] = char.ToUpper(enterInCharArray[i+2]);
+                }
+            }
+
+            Console.WriteLine(string.Join("", enterInCharArray));
         }
     }
 }
+
+
