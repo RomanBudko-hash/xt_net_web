b07bf219b0d03939c2192557e2a5b99790b2dfc0
RomanBudko <romanjaneiro@mail.ru>
2020-05-28 22:48:25 +0400

    Создание нового проекта для первого задания

---
+++ Task 2.1 OOP (OKAY-OKAY, PROGRAMMING)/2.1.1. CUSTOM STRING/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1._1.CUSTOM_STRING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
