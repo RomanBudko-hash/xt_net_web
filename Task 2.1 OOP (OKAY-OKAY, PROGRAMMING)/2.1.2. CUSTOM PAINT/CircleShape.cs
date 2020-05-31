92dbb36bf93d1a70e8bef9c2cbf4a4791a9fd3f4
RomanBudko <romanjaneiro@mail.ru>
2020-05-31 23:02:24 +0400

    Rename classes, add base of Figure class

--- Task 2.1 OOP (OKAY-OKAY, PROGRAMMING)/2.1.2. CUSTOM PAINT/CircleShape.cs
+++ Task 2.1 OOP (OKAY-OKAY, PROGRAMMING)/2.1.2. CUSTOM PAINT/CircleShape.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _2._1._2.CUSTOM_PAINT
 {
-    abstract class Figure
+    class CircleShape : Figure // Окружность
     {
 
     }
---
+++ Task 2.1 OOP (OKAY-OKAY, PROGRAMMING)/2.1.2. CUSTOM PAINT/Figure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1._2.CUSTOM_PAINT
+{
+    abstract class Figure
+    {
+        private double x; // Coordinate of point being
+        private double y;
+
+        public Figure (double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        abstract public int MethodForFindArea(); 
+    }
+}
