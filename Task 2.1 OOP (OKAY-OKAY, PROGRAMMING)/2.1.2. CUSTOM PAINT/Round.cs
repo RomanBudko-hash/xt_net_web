82d218a21e043f32790891a309066bb8df3a7faa
RomanBudko <romanjaneiro@mail.ru>
2020-06-06 01:20:15 +0400

    Add new classes

--- Task 2.1 OOP (OKAY-OKAY, PROGRAMMING)/2.1.2. CUSTOM PAINT/Round.cs
+++ Task 2.1 OOP (OKAY-OKAY, PROGRAMMING)/2.1.2. CUSTOM PAINT/Round.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _2._1._2.CUSTOM_PAINT
 {
 	public class Round : CircleShape
 	{
 		public int area { get; }
+
+		public CircleShape(int x, int y, int insideRadius) : base(x, y, insideRadius)
+		{
+			this.insideRadius = insideRadius;
+		}
 
 		public void FindToArea(int insideRadius)
         {
---
+++ Task 2.1 OOP (OKAY-OKAY, PROGRAMMING)/2.1.2. CUSTOM PAINT/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1._2.CUSTOM_PAINT
+{
+	public class Square : Figure
+	{
+		public int side { get; }
+
+		public CircleShape(int x, int y, int side) : base(x, y, side)
+		{
+			this.side = side;
+		}
+
+	}
+}
