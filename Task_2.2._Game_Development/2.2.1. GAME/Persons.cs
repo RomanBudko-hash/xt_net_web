c98971c01f02f195f8d98c3e107fa9f357bc2b8e
RomanBudko <romanjaneiro@mail.ru>
2020-06-17 01:14:26 +0400

    Added the main classes

---
+++ Task_2.2._Game_Development/2.2.1. GAME/Persons.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2._1.GAME
+{
+    public abstract class Persons
+    {
+        public int HealthOfSomethingAlive { get; set; } = 100;
+
+        private int SpeedOfSomethingAlive;
+
+        public int Speed
+        {
+            get => SpeedOfSomethingAlive;
+
+            set
+            {
+                if (Speed > 0 && Speed <= 100)
+                {
+                    SpeedOfSomethingAlive = Speed;
+                }
+            }
+        }
+
+        private int DamageOfSomethingAlive;
+
+        public int Damage
+        {
+            get => DamageOfSomethingAlive;
+
+            set
+            {
+                if (Damage > 0 && Damage <= 50)
+                {
+                    DamageOfSomethingAlive = Damage;
+                }
+            }
+        }
+
+        public Persons(int Damage, int Speed)
+        {
+            DamageOfSomethingAlive = Damage;
+            SpeedOfSomethingAlive = Speed;
+        }
+
+        public abstract int ToGiveDamage();
+
+        public abstract int GetHealth();
+    }
+}
---
+++ Task_2.2._Game_Development/2.2.1. GAME/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2._1.GAME
+{
+    public class Player : Persons
+    {
+        public Player(int Damage, int Speed) : base (Damage, Speed)
+        {
+            
+        }
+    }
+}
