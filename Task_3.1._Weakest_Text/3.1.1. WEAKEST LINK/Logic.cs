c98022c59845ee3ce682ca52acc563915aee511e
RomanBudko <romanjaneiro@mail.ru>
2020-06-27 22:30:17 +0400

    Main functions is finished

---
+++ Task_3.1._Weakest_Text/3.1.1. WEAKEST LINK/Logic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _3._1._1.WEAKEST_LINK
+{
+    public class Logic
+    {
+        private int peopleCount;
+
+        public int PeopleCount
+        {
+
+            get => peopleCount;
+
+            set
+            {
+                if(value < 1)
+                {
+                    Console.WriteLine("Please, retry enter");
+                }
+                else
+                {
+                    peopleCount = value;
+                }
+            }
+        }
+
+        public void PeopleCrossOut(int N)
+        {
+            int round = 1;
+
+            int summOfPeople = N; // needed to be split variable
+
+            do
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        --summOfPeople;
+                    }
+                }
+                Console.WriteLine($"Раунд {round}. Вычеркнут человек(-и). Людей осталось: {summOfPeople}");
+                N = summOfPeople;
+                round++;
+            }
+            while (summOfPeople != 1);
+        }
+    }
+}
---
+++ Task_3.1._Weakest_Text/3.1.1. WEAKEST LINK/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _3._1._1.WEAKEST_LINK
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic play = new Logic();
+
+            int x = play.PeopleCount = 20;
+
+            play.PeopleCrossOut(x);
+        }
+    }
+}
