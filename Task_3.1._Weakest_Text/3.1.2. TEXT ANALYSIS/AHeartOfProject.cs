2239614a7831143fee4a0cb4d41f5e82d40b5ae8
RomanBudko <romanjaneiro@mail.ru>
2020-07-01 01:20:12 +0400

    Need complete

---
+++ Task_3.1._Weakest_Text/3.1.2. TEXT ANALYSIS/AHeartOfProject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._1._2.TEXT_ANALYSIS
+{
+    public class AHeartOfProject
+    {
+        public string Titul { get; } = "Here must to be your text";
+
+        public string[] CheckPhrase(string example)
+        {
+            try
+            {
+                example = example.ToLower();
+            }
+            catch
+            {
+                //Exception
+            }
+
+            string[] castString = example.Split('_', '-', ':', ';', '.', ',', '!', '?', ' ');
+
+            return castString;
+        }
+
+        public Dictionary<string, int> FindRetryWords(string [] castString)
+        {
+            Dictionary<string, int> saveResult = new Dictionary<string, int>();
+
+            for (int i = 0; i < castString.Length; i++) // Run 
+            {
+                int count = 0;
+
+                for (int j = i + 1; j < castString.Length; j++)
+                {
+                    if (castString[i].Equals(castString[j])
+                        && castString[i] != ""
+                        && castString[j] != "")
+                    {
+                        ++count;
+                    }
+                }
+
+                if (!saveResult.ContainsKey(castString[i]))
+                {
+                    saveResult.Add(castString[i], count);
+                }
+            }
+            return saveResult;
+        }
+    }
+}
+
--- Task_3.1._Weakest_Text/3.1.2. TEXT ANALYSIS/Program.cs
+++ Task_3.1._Weakest_Text/3.1.2. TEXT ANALYSIS/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace _3._1._2.TEXT_ANALYSIS
+﻿using System;
+namespace _3._1._2.TEXT_ANALYSIS
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            AHeartOfProject aHeart = new AHeartOfProject();
+
+            Console.WriteLine(aHeart.Titul.PadLeft(50));
+
+            string example = Console.ReadLine();
+
+            Console.WriteLine(string.Join(" ", aHeart.FindRetryWords(aHeart.CheckPhrase(example))));
+            
         }
     }
 }
