7bc305620c20571733bccc006270a7977fa96988
Роман Будко <romanjaneiro@mail.ru>
2020-07-15 04:10:32 +0400

    Need to add the last method

---
+++ Task_3.3._Pizza_Time/⦁ SUPER ARRAY/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace __SUPER_ARRAY
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] NumbersArray = { 1, 2, 3, 12, 4, 5, 1 };
+
+            NumbersArray.ForeachItem( a => a * 3);
+
+            Console.WriteLine(NumbersArray.SumOfItem() + "\n");
+
+            Console.WriteLine(NumbersArray.AverageOfItem() + "\n");
+
+            Console.WriteLine(NumbersArray.OftenRetry() + "\n");
+
+            foreach (var item in NumbersArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    public static class ArrayExtension
+    {
+        public static void ForeachItem(this int [] array, Func<int, int> func)
+        {
+            if (array is null || array.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = func(array[i]);
+                }
+            }
+        }
+
+        public static int SumOfItem(this int[] array)
+        {
+            if (array is null || array.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                return array.Sum();
+            }
+        }
+
+        public static double AverageOfItem(this int[] array)
+        {
+            if (array is null || array.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                return array.Average();
+            }
+        } 
+
+        public static int OftenRetry(this int[] array)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Task_3.3._Pizza_Time/⦁ SUPER ARRAY/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("⦁ SUPER ARRAY")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("⦁ SUPER ARRAY")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c5e21027-5d32-4b34-a5b9-d7fc153b86f2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
