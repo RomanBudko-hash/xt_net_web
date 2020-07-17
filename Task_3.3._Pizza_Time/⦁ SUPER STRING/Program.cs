82744873386898693c33e24bd2011f9fc1f76a55
Роман Будко <romanjaneiro@mail.ru>
2020-07-18 02:04:52 +0400

    Done

---
+++ Task_3.3._Pizza_Time/⦁ SUPER STRING/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+
+namespace __SUPER_STRING
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string SomeWord = "5sывыв";
+
+            System.Console.WriteLine(SomeWord.WordLanguage());
+        }
+    }
+
+    public static class Extension
+    {
+        public static string WordLanguage(this string word)
+        {
+            int rus = 0, eng = 0, dig = 0;
+
+            if (Regex.IsMatch(word, "[а-яА-ЯеЁ]"))
+            {
+                rus += 1;
+            }
+            if (Regex.IsMatch(word, "[a-zA-Z]"))
+            {
+                eng += 1;
+            }
+            if (Regex.IsMatch(word, @"[\d\p{Z}]"))
+            {
+                dig += 1;
+            }
+            if (rus + eng + dig > 1)
+            {
+                return "Mixed";
+            }
+
+            if (rus == 1)
+                return "Russian";
+            if (eng == 1)
+                return "English";
+            if (dig == 1)
+                return "Digit";
+
+            return "No one from the variants";
+        }
+    }
+}
